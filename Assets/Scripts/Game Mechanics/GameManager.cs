ad2f8e5ff8719d0ecbce8be9c1dc5b140549debc
DanilGolovanov <golovanov.da1@gmail.com>
2020-11-25 23:31:08 +1100

    added player movement, player stats, not yet functional inventory and equipment systems

---
+++ Assets/Scripts/Game Mechanics/GameManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public float currentHealth = 100;
+    public float maxHealth = 100;
+    public static float currentStamina = 100;
+    public static float maxStamina = 100;
+    public static float currentMana = 100;
+    public static float maxMana = 100;
+
+    [SerializeField]
+    private GameObject deathScreen;
+    [SerializeField]
+    private PlayerMovement player;
+    private Camera fpsCamera;
+
+    public GameObject currentCheckpoint;
+
+    [SerializeField]
+    private float respawnDelay = 0;
+    private bool coroutineIsRunning = false;
+
+    [SerializeField]
+    private AudioSource deathSound;
+    [SerializeField]
+    private AudioSource respawnSound;
+
+    public bool playerIsAlive = true;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        fpsCamera = player.GetComponentInChildren<Camera>();
+    }
+
+    private void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            if (playerIsAlive)
+            {
+                deathSound.Play();
+            }
+            playerIsAlive = false;            
+            deathScreen.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            fpsCamera.gameObject.SetActive(false);
+        }
+    }
+
+    public void RespawnPlayer()
+    {
+        if (!coroutineIsRunning)
+        {
+            StartCoroutine("RespawnPlayerCo");
+        }
+    }
+
+    public IEnumerator RespawnPlayerCo()
+    {
+        coroutineIsRunning = true;
+
+        yield return new WaitForSeconds(respawnDelay);
+
+        currentHealth = maxHealth;
+        currentMana = maxMana;
+        currentStamina = maxStamina;
+
+        deathScreen.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+
+        player.transform.position = new Vector3(currentCheckpoint.transform.position.x, 
+                                                currentCheckpoint.transform.position.y + 5, 
+                                                currentCheckpoint.transform.position.z);
+        fpsCamera.gameObject.SetActive(true);
+        coroutineIsRunning = false;
+        respawnSound.Play();
+        playerIsAlive = true;
+    }
+}
---
+++ Assets/Scripts/Inventory System/Inventory/DisplayInventory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayInventory : MonoBehaviour
+{
+    public InventoryObject inventory;
+
+    public int xStart;
+    public int yStart;
+
+    public int xSpaceBetweenItems;
+    public int numberOfColumns;
+    public int ySpaceBetweenItems;
+    Dictionary<InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventorySlot, GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateDisplay();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateDisplay();
+    }
+
+    public void UpdateDisplay()
+    {
+        for (int i = 0; i < inventory.Container.Count; i++)
+        {
+            if (itemsDisplayed.ContainsKey(inventory.Container[i]))
+            {
+                itemsDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+            }
+            else
+            {
+                var obj = CreateInventoryItem(i);
+                itemsDisplayed.Add(inventory.Container[i], obj);
+            }
+        }
+    }
+
+    public void CreateDisplay()
+    {
+        for (int i = 0; i < inventory.Container.Count; i++)
+        {
+            CreateInventoryItem(i);
+        }
+    }
+
+    private GameObject CreateInventoryItem(int index)
+    {
+        var obj = Instantiate(inventory.Container[index].item.prefab, Vector3.zero, Quaternion.identity, transform);
+        obj.GetComponent<RectTransform>().localPosition = GetPosition(index);
+        // format with commas to make it look better when there are more than 1k items
+        obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[index].amount.ToString();
+        return obj;
+    }
+
+    private Vector3 GetPosition(int index)
+    {
+        return new Vector3(xStart + (xSpaceBetweenItems * (index % numberOfColumns)), yStart + ((-ySpaceBetweenItems * (index / numberOfColumns))), 0f);
+    }
+}
